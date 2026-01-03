15ab651a36d1eeb3441b8884e3ee582dfa0fb014
yudnata <yudhinata04@gmail.com>
2026-01-03 22:27:06 +0800

    first setup

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend Running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
